4368237454fc5468d57bb21dc2fa1477502f983d
Jay2113 <jay.ambrish.raval@gmail.com>
2024-07-12 17:25:52 -0400

    add a static page

---
+++ app/static/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const StaticPage = () => {  
+    return (
+      <div>
+        <h1>Static page</h1>
+        <p>This is a static page built with Next.js.</p>      
+    </div>
+    );
+  };
+  
+  export default StaticPage;
+  
